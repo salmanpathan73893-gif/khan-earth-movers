a2bc4e0bada50fde80827912d585a67b4882da9f
Salman Khan <salmanpathan73893@gmail.com>
2026-07-20 21:57:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("KHAN EARTH MOVERS Website Loaded");
+<div class="card">
+<img src="images/newproduct.jpg">
+<h4>Product Name</h4>
+<p>Product details</p>
+<a href="https://wa.me/916397684170">
+Enquire Now
+</a>
+</div>
